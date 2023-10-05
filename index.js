160d2881e688a5c19f73bc8ac09775a8f5a0d00b
Andrew Faryniarz <afaryniarz3@gmail.com>
2023-10-05 16:18:05 -0400

    commit tree b4e372c394a3df5bcfa92e1c7d31ba5a7506859e

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 function fetchBooks() {
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => {
+    console.log(json)
+  renderBooks(json)
+  }
+    
+    );
+
+}
   // To pass the tests, don't forget to return your fetch!
   
-}
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
+    console.log(book.name)
     main.appendChild(h2);
   });
+}
+
+function getBookNames(book) {
+  book.name
 }
 
 document.addEventListener('DOMContentLoaded', function() {
